23f4b7563f324529a380bedd348944b3de34ca9e
Anda <anda.epure.20@ucl.ac.uk>
2024-03-18 09:45:31 +0000

    Update setup.sh file for mac

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-wget http://www.iro.umontreal.ca/~lisa/icml2007data/mnist_rotation_new.zip
+#wget http://www.iro.umontreal.ca/~lisa/icml2007data/mnist_rotation_new.zip
+curl http://www.iro.umontreal.ca/~lisa/icml2007data/mnist_rotation_new.zip --output mnist_rotation_new.zip
 unzip mnist_rotation_new.zip
 rm mnist_rotation_new.zip
